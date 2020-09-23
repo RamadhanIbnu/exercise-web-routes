e054051d2b4052bad30a919beea0d58691b76ef1
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-23 23:13:45 +0700

    adding calculatorBMI

--- src/pages/CalculatorBMI.js
+++ src/pages/CalculatorBMI.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Container, Card, Form, Button } from "react-bootstrap";
+import '../style/CalculatorBMI.css';
+import swal from '@sweetalert/with-react'
 
 const Calculator = () =>{
+
+    const [weight, setWeight] = useState();
+    const [height, setHeight] = useState()
+
+    const handleSubmit = (event)=>{
+        swal(
+                <div>
+                  <h1>Data anda :</h1>
+                  <p>Your BMI: {weight/(height*height)}</p>
+                </div> 
+            )
+        event.preventDefault();
+    }
+
     return(
-        <div>
-            <h1>Ini adalah halaman untuk calculator BMI</h1>
-        </div>
+        <Container>
+            <Card className="card-container" >
+                <h1>Check Your Body Mass Index</h1>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Weight (Kg)</Form.Label>
+                        <Form.Control type="number" className="input-weight" onChange={(event)=>setWeight(event.target.value)}></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Height (m)</Form.Label>
+                        <Form.Control type="number" className="input-weight" onChange={(event)=>setHeight(event.target.value)}></Form.Control>
+                    </Form.Group>
+                    <Button variant="primary" type="submit"> Submit </Button>
+                </Form>
+            </Card>
+        </Container>
     )
 }
 
