48151eb0efc094bb37fc64b86d9bdda6649153d5
RamadhanIbnu <ramadhanibnu77@gmail.com>
2020-09-29 22:00:33 +0700

    adding page TextTransform

--- src/pages/TextTransform.js
+++ src/pages/TextTransform.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Container, Card, Form, Alert} from "react-bootstrap";
+import '../style/TextTransform.css'
 
 const TextTransform = () =>{
+    const [text, setText] = useState("")
+
+    const handleChange = (event) =>{
+        setText(event.target.value.toUpperCase())
+    }
+
     return(
-        <div>
-            <h1>Ini adalah halaman untuk text transform</h1>
-        </div>
+        <Container>
+            <Card>
+                <Form.Group className="container-output">
+                    <Form.Label className="header-output">
+                        <h1>Here Your Text</h1>
+                    </Form.Label>
+                        <Alert variant="info" className="output-text">{text}</Alert>
+                </Form.Group>
+                <hr/>
+                <Form.Group className="header-input">
+                    <Form.Label>Input Your Text</Form.Label>
+                    <Form.Control className="control" type="text" onChange={handleChange} placeholder="input your text here" />
+                </Form.Group>
+            </Card>
+        </Container>
     )
 }
 
